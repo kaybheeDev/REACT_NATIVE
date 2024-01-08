f8be81b66620c32b247ceb4bbfe41260fd9c9e98
kaybheeDev <abdulkabiropeyemi7@gmail.com>
2024-01-08 15:14:52 +0100

    all

---
+++ metro.config.js
@@ -0,0 +1,11 @@
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig();
+
+  return {
+    resolver: {
+      sourceExts: [...defaultConfig.resolver.sourceExts, 'dotenv'],
+    },
+  };
+})();
